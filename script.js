9d732066ef89bc8ba7e4e460d9b0be135ef939f7
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-07-14 19:49:19 -0500

    Created initial file structure.

---
+++ script.js
@@ -0,0 +1,35 @@
+const WORLD_WIDTH = 100
+const WORLD_HEIGHT = 30
+
+const worldElem = document.querySelector('[data-world]')
+
+setPixelToWorldScale()
+window.addEventListener("resize", setPixelToWorldScale)
+
+let lastTime
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+window.requestAnimationFrame(update)
+
+function setPixelToWorldScale() {
+    let worldToPixelScale
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT )
+    {
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    }
+
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+
+}
